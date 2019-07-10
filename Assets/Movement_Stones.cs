26113c9ca495ca495d709cebced7b1f68b2dbb20
Unknown <kmk@gmx.de>
2019-07-10 20:15:31 +0200

    Movement of stones

---
+++ Assets/Movement_Stones.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_Stones : MonoBehaviour {
+
+    Rigidbody rigid;
+    float speed = 1;
+    Vector3 dir = Vector3.zero;
+    float deltaCounter = 0;
+
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody>();
+        speed = 60;
+        dir = new Vector3(-1,0, 0);
+        deltaCounter += Random.Range(0, 7) * 0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        dir.y = Mathf.Sin(deltaCounter)/3;
+        rigid.velocity = dir * speed * Time.deltaTime;
+        deltaCounter += Time.deltaTime*2;
+    }
+}
