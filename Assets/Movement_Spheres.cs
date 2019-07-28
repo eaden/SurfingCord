75a70144b45d39418921d008d98153c8e951d6ac
Unknown <kmk@gmx.de>
2019-07-28 18:20:35 +0200

    Spheres function correctly now

---
+++ Assets/Movement_Spheres.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_Spheres : MonoBehaviour {
+
+
+    Rigidbody rigid;
+    float speed = 1;
+    Vector3 dir;
+
+    // Use this for initialization
+    void Start () {
+        rigid = GetComponent<Rigidbody>();
+        dir = new Vector3(-1, 0, 0);
+        speed = 60;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rigid.velocity = dir * speed * Time.deltaTime;
+    }
+}
